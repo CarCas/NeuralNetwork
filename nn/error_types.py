Fix error calculation

nn/error_types.py
@@ -15,40 +15,32 @@ class ErrorComputation:
     def __init__(self, identifier: ErrorTypes):
         self.identifier: ErrorTypes = identifier
 
-    def __call__(self, d: Sequence[float], out: Sequence[float]) -> Sequence[float]:
+    def __call__(self, d: Sequence[float], out: Sequence[float]) -> float:
         if self.identifier == ErrorTypes.MSE:
             return np.array(self.mean_square_error(d, out))
         elif self.identifier == ErrorTypes.MEE:
             return np.array(self.mean_euclidean_error(d, out))
-        elif self.identifier == ErrorTypes.MAE:
-            return np.array(self.mean_absolute_error(d, out))
         elif self.identifier == ErrorTypes.MIS:
             return np.array(self.mismatch_error(d, out))
         elif self.identifier == ErrorTypes.ACC:
             return self.accuracy(d, out)
         return np.array(-1)
 
-    def post(self, error: Sequence[float], len: int) -> Sequence[float]:
-        if self.identifier == ErrorTypes.MEE:
-            error = np.sqrt(error)
+    def post(self, error: float, len: int) -> float:
         return np.true_divide(error, len)
 
     @staticmethod
-    def mean_square_error(d: Sequence[float], out: Sequence[float]) -> Sequence[float]:
-        return np.square(np.subtract(d, out))
+    def mean_square_error(d: Sequence[float], out: Sequence[float]) -> float:
+        return np.sum(np.square(np.subtract(d, out)))
 
     @staticmethod
-    def mean_euclidean_error(d: Sequence[float], out: Sequence[float]) -> Sequence[float]:
-        return np.square(np.subtract(d, out))
+    def mean_euclidean_error(d: Sequence[float], out: Sequence[float]) -> float:
+        return np.linalg.norm(np.subtract(d, out), 2)
 
     @staticmethod
-    def mean_absolute_error(d: Sequence[float], out: Sequence[float]) -> Sequence[float]:
-        return np.abs(np.subtract(d, out))
+    def mismatch_error(d: Sequence[float], out: Sequence[float]) -> float:
+        return np.not_equal(d, np.round(out)).any().astype(float)
 
     @staticmethod
-    def mismatch_error(d: Sequence[float], out: Sequence[float]) -> Sequence[float]:
-        return np.not_equal(d, np.round(out)).astype(float)
-
-    @staticmethod
-    def accuracy(d: Sequence[float], out: Sequence[float]) -> Sequence[float]:
-        return np.equal(d, np.round(out)).astype(float)
+    def accuracy(d: Sequence[float], out: Sequence[float]) -> float:
+        return np.equal(d, np.round(out)).all().astype(float)

nn/neural_network.py
@@ -99,8 +99,8 @@ class NeuralNetwork:
 
         self.out: Sequence[float] = []
 
-        self.training_errors: defaultdict[ErrorTypes, MutableSequence[Sequence[float]]] = defaultdict(lambda: [])
-        self.testing_errors: defaultdict[ErrorTypes, MutableSequence[Sequence[float]]] = defaultdict(lambda: [])
+        self.training_errors: defaultdict[ErrorTypes, MutableSequence[float]] = defaultdict(lambda: [])
+        self.testing_errors: defaultdict[ErrorTypes, MutableSequence[float]] = defaultdict(lambda: [])
 
     def set(self, **kwargs) -> NeuralNetwork:
         self.__dict__.update(**kwargs)
@@ -143,9 +143,9 @@ class NeuralNetwork:
         self,
         patterns: Sequence[Pattern],
         error_type: Optional[ErrorTypes] = None
-    ) -> Sequence[float]:
+    ) -> float:
         if not len(patterns):
-            return []
+            return np.nan
 
         if error_type is None:
             error_type = self.error_types[0]
@@ -171,7 +171,7 @@ class NeuralNetwork:
     #     return error
 
     @property
-    def current_training_error(self) -> Sequence[float]:
+    def current_training_error(self) -> float:
         return self.training_errors[self.error_types[0]][-1]
 
     def _early_stopping(self) -> bool: