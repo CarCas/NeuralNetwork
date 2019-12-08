dbef8f3ad77bc7fe785c5cc7820c09b4d70db032
William Guglielmo <william@deselmo.com>
2019-12-08 07:07:19 +0100

    Use fixed size lists for feedforward and train

--- nn/architectures/multilayer_perceptron/neural_network.py
+++ nn/architectures/multilayer_perceptron/neural_network.py
@@ -18,25 +18,45 @@ class MLPNeuralNetwork(BaseNeuralNetwork):
         self.activation_hidden: ActivationFunction = activation_hidden
         self.eta: float = eta
 
+        self.len_layers = len(layers)
+        self.inputs = [np.array(0)] * self.len_layers
+        self.outputs = [np.array(0)] * self.len_layers
+
+        self.deltas = [np.array(0)] * self.len_layers
+        self.gradients = [np.array(0)] * self.len_layers
+
     def __call__(self, *input: Sequence[float]) -> Sequence[Sequence[float]]:
-        self.inputs = [np.insert(input, 0, 1, 1)]
-        self.outputs = []
-        for layer in self.layers[:-1]:
-            self.outputs.append(self.activation_hidden(self.inputs[-1] @ layer.T))
-            self.inputs.append(np.insert(self.outputs[-1], 0, 1, 1))
-        self.outputs.append(self.activation(self.inputs[-1] @ self.layers[-1].T))
-        return self.outputs[-1]
+        layers = self.layers
+        inputs = self.inputs
+        outputs = self.outputs
+        len_layers = self.len_layers
+        activation_hidden = self.activation_hidden
+
+        inputs[0] = np.insert(input, 0, 1, 1)
+        for i in range(len_layers-1):
+            outputs[i] = activation_hidden(inputs[i] @ layers[i].T)
+            inputs[i+1] = np.insert(outputs[i], 0, 1, 1)
+        outputs[-1] = self.activation(inputs[-1] @ layers[-1].T)
+        return outputs[-1]
 
     def train(self, patterns: Sequence[Pattern]) -> None:
+        layers = self.layers
+        inputs = self.inputs
+        outputs = self.outputs
+        deltas = self.deltas
+        gradients = self.gradients
+        activation_hidden = self.activation_hidden
+        eta = self.eta
+
         x, d = zip(*patterns)
         self(*x)
 
-        delta = [(d - self.outputs[-1]) * self.activation.derivative(self.outputs[-1])]
-        for i in range(len(self.layers))[::-1][1:]:
-            delta.insert(0, (
-                np.dot(self.layers[i+1].T[1:], delta[0].T).T * self.activation_hidden.derivative(self.outputs[i])))
+        deltas[-1] = (d - outputs[-1]) * self.activation.derivative(outputs[-1])
+        for i in range(self.len_layers)[-2::-1]:
+            deltas[i] = np.multiply(np.matmul(layers[i+1].T[1:],
+                                              deltas[i+1].T).T,
+                                    activation_hidden.derivative(outputs[i]))
 
-        self.gradients = []
-        for i in range(len(self.layers)):
-            self.gradients.append(np.mean((delta[i] * self.inputs[i][np.newaxis].T).T, axis=1))
-            self.layers[i] += self.eta * self.gradients[i]
+        for i in range(self.len_layers):
+            gradients[i] = np.mean((deltas[i] * inputs[i][np.newaxis].T).T, axis=1)
+            layers[i] += eta * gradients[i]
