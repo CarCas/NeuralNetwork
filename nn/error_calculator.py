6dabdaf7a8ac72d6e0bbedd2910023f0767c0fd9
William Guglielmo <william@deselmo.com>
2019-12-08 02:39:40 +0100

    Type error_function

--- nn/error_calculator.py
+++ nn/error_calculator.py
@@ -1,25 +1,35 @@
-from typing import Sequence, Callable
+from typing import Sequence, Callable, NamedTuple
 import enum
 import numpy as np
 
 from nn.types import Pattern, BaseNeuralNetwork
-from collections import namedtuple
 
 
-f_container = namedtuple('f_container', ['f'])
+ErrorFunction = Callable[[Sequence[BaseNeuralNetwork], Sequence[Sequence[float]]], float]
+
+
+class ErrorFunctionContainer(NamedTuple):
+    f: ErrorFunction
 
 
 class ErrorCalculator(enum.Enum):
-    MSE = f_container(lambda d, out: np.mean(np.sum(np.square(np.subtract(d, out)), axis=1)))
-    MEE = f_container(lambda d, out: np.mean(np.linalg.norm(np.subtract(d, out), axis=1)))
-    MIS = f_container(lambda d, out: np.mean(np.not_equal(d, np.round(out)).astype(float)))
-    ACC = f_container(lambda d, out: np.mean(np.equal(d, np.round(out)).astype(float)))
+    # mean square error
+    MSE = ErrorFunctionContainer(lambda d, out: np.mean(np.sum(np.square(np.subtract(d, out)), axis=1)))
+
+    # mean euclidean error
+    MEE = ErrorFunctionContainer(lambda d, out: np.mean(np.linalg.norm(np.subtract(d, out), axis=1)))
+
+    # mismatch
+    MIS = ErrorFunctionContainer(lambda d, out: np.mean(np.not_equal(d, np.round(out)).astype(float)))
+
+    # accuracy
+    ACC = ErrorFunctionContainer(lambda d, out: np.mean(np.equal(d, np.round(out)).astype(float)))
 
     def __call__(
         self,
         learning_networks: Sequence[BaseNeuralNetwork],
         patterns: Sequence[Pattern]
     ) -> Sequence[float]:
-        error_function = self.value.f
+        error_function: ErrorFunction = self.value.f
         x, d = zip(*patterns)
         return [error_function(d, nn(*x)) for nn in learning_networks]
