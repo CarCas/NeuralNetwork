86ffcc96b1e8a6af919d87750c1af41c4d5a74bb
William Guglielmo <william@deselmo.com>
2020-01-26 22:53:34 +0100

    Add monk1-1 gridsearch

--- nn/playground/monk1-1.py
+++ nn/playground/monk1-1.py
@@ -20,33 +20,25 @@ params_nn: Dict[str, Sequence[Any]] = dict(
 params_architecture: Mapping[str, Sequence[Any]] = dict(
     size_hidden_layers=[(2,)],
     activation=[sigmoid, tanh_classification],
-    activation_hidden=[relu, tanh],
+    activation_hidden=[sigmoid],
     eta=[0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
     alpha=[0, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
     alambd=[0, 0.0001, 0.001],
     eta_decay=[0, 0.001],
 )
-
 cv_params: Mapping[str, Any] = dict(
     cv=10,
     error_calculator=ErrorCalculator.ACC,
     to_shuffle=True,
 )
 
-# validation_params: Mapping[str, Any] = dict(
-#     validation_set=test_data,
-#     error_calculator=ErrorCalculator.ACC,
-#     to_shuffle=True,
-# )
-
 grid_search_results = grid_search(
     train_data,
     params_nn=params_nn,
     params_architecture=params_architecture,
     cv_params=cv_params,
-    # validation_params=validation_params,
 
-    n_jobs=7,
+    n_jobs=8,
 )
 
-write_on_file(grid_search_results[::-1], 'results/monk1')
+write_on_file(grid_search_results[::-1], 'results/monk1-1')
