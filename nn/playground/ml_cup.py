283b23bf0b69eda0eb6927747a441d51940b2124
William Guglielmo <william@deselmo.com>
2020-01-30 02:46:45 +0100

    Add gridsearch for ml_cup normalized

--- nn/playground/ml_cup.py
+++ nn/playground/ml_cup.py
@@ -9,41 +9,48 @@ import numpy as np
 
 ml_cup_training_dataset = read_ml_cup_tr_normalized()
 train_data, test_set = split_dataset(ml_cup_training_dataset, to_shuffle=True, seed=0)
+train_set, validation_set = split_dataset(train_data, percentage=2/3, to_shuffle=True, seed=0)
 
 np.random.seed()
 
 params_nn: Dict[str, Sequence[Any]] = dict(
-    error_calculator=[ErrorCalculator.MSE],
+    error_calculator=[ErrorCalculator.MEE],
     learning_algorithm=[minibatch(0.05)],
-    epochs_limit=[1000],
-    n_init=[3],
+    epochs_limit=[500],
+    n_init=[1],
     epsilon=[0],
     patience=[10],
 )
 
 params_architecture: Mapping[str, Sequence[Any]] = dict(
-    size_hidden_layers=[(2,), (4,), (2, 2)],
-    activation=[tanh_classification],
-    activation_hidden=[relu],
-    eta=[0.1, 0.01, 0.001],
-    alpha=[0, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
-    alambd=[0, 0.0001, 0.001, 0.01],
+    size_hidden_layers=[[100], [50, 50], [50, 20], [50, 50, 50], [50, 50, 20]],
+    activation=[identity],
+    activation_hidden=[sigmoid, tanh, relu],
+    eta=[0.1, 0.01, 0.001, 0.0001],
+    alpha=[0, 0.5, 0.75, 0.9],
+    alambd=[0, 0.00001, 0.0001, 0.001],
     eta_decay=[0],
 )
 
 cv_params: Mapping[str, Any] = dict(
     cv=3,
-    error_calculator=ErrorCalculator.MSE,
+    error_calculator=ErrorCalculator.MEE,
     to_shuffle=True,
+)
+
+validation_params: Mapping[str, Any] = dict(
+    validation_set=validation_set,
+    error_calculator=ErrorCalculator.MEE,
 )
 
 grid_search_results = grid_search(
     train_data,
     params_nn=params_nn,
     params_architecture=params_architecture,
-    cv_params=cv_params,
+    # cv_params=cv_params,
+    validation_params=validation_params,
 
-    n_jobs=8,
+    n_jobs=1,
 )
 
-write_on_file(grid_search_results, 'results/monk3-mse-low_eta')
+write_on_file(grid_search_results, 'results/ml_cup_nocv')
--- nn/playground/ml_cup_plot.py
+++ nn/playground/ml_cup_plot.py
@@ -1,26 +1,29 @@
 from nn import *
 import numpy as np
-from nn.playground.utilities import read_ml_cup_tr, plot
+from nn.playground.utilities import read_ml_cup_tr_normalized, plot
 
 
 if __name__ == '__main__':
-    ml_cup_training_dataset = read_ml_cup_tr()
+    ml_cup_training_dataset = read_ml_cup_tr_normalized()
+
     train_data, test_data = split_dataset(ml_cup_training_dataset, to_shuffle=True, seed=0)
     train_set, validation_set = split_dataset(train_data, percentage=2/3, to_shuffle=True, seed=0)
 
     seed = 0
     learning_algorithm = minibatch(0.05)
-    epochs_limit = 10000
-    size_hidden_layers = [100]
-    activation_hidden = sigmoid
-    eta = 0.1
-    alpha = 0.3
-    alambd = 0.0005
+    epochs_limit = 1000
+    # learning_algorithm = batch
+    # epochs_limit = 100
+    size_hidden_layers = [50, 50]
+    activation_hidden = relu
+    eta = 0.01
+    alpha = 0.5
+    alambd = 0
 
     nn = NeuralNetwork(
         seed=seed,
         epochs_limit=epochs_limit,
-        learning_algorithm=batch,
+        learning_algorithm=learning_algorithm,
         n_init=1,
         epsilon=0,
         error_calculator=ErrorCalculator.MEE,
@@ -34,15 +37,15 @@ if __name__ == '__main__':
         ),
     )
 
-    validation_result = validation(nn, train_set, validation_set, ErrorCalculator.MEE, True)
+    validation_result = validation(nn, train_set, validation_set, ErrorCalculator.MEE, False)
 
     print(validation_result)
 
-    training_curve = nn.training_curve
-    validation_curve = nn.validation_curve
+    # training_curve = nn.training_curve
+    # validation_curve = nn.validation_curve
 
-    nn.error_calculator = ErrorCalculator.MEE
-    print('mee', nn.compute_error(train_set), nn.compute_error(validation_set))
+    # nn.error_calculator = ErrorCalculator.MEE
+    # print('mee', nn.compute_error(train_set), nn.compute_error(validation_set))
 
-    nn.error_calculator = ErrorCalculator.MEE
-    plot(training_curve, validation=validation_curve)
+    # nn.error_calculator = ErrorCalculator.MEE
+    # plot(training_curve, validation=validation_curve)
