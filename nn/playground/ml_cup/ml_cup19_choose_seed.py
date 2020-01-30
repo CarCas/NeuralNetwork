98d9fa4940ffe389cd1246376825a1d2ffafd8b3
William Guglielmo <william@deselmo.com>
2020-01-30 09:36:38 +0100

    Update results

--- nn/playground/ml_cup/ml_cup19_choose_seed.py
+++ nn/playground/ml_cup/ml_cup19_choose_seed.py
@@ -6,26 +6,26 @@ from nn.utilities import read_ml_cup_tr, plot
 
 ml_cup_training_dataset = read_ml_cup_tr()
 train_data, test_set = split_dataset(ml_cup_training_dataset, to_shuffle=True, seed=0)
-train_set, validation_set = split_dataset(train_data, percentage=2/3, to_shuffle=True, seed=0)
+train_set, validation_set = split_dataset(train_data, percentage=1/2, to_shuffle=True, seed=0)
 
 epsilon = 0
 
 params_nn: Dict[str, Sequence[Any]] = dict(
-    seed=list(range(0, 8)),
+    seed=list(range(0, 24)),
     error_calculator=[ErrorCalculator.MEE],
-    learning_algorithm=[batch],
-    epochs_limit=[13000],
+    learning_algorithm=[minibatch(0.05)],
+    epochs_limit=[10000],
     n_init=[1],
     epsilon=[0],
     patience=[10],
 )
 params_architecture: Mapping[str, Sequence[Any]] = dict(
-    size_hidden_layers=[[40, 30]],
+    size_hidden_layers=[[75, 75]],
     activation=[identity],
     activation_hidden=[tanh],
-    eta=[0.007],
-    alpha=[0.55],
-    alambd=[1e-05],
+    eta=[0.0085],
+    alpha=[0.6],
+    alambd=[0],
     eta_decay=[0],
 )
 
@@ -43,4 +43,4 @@ grid_search_results = grid_search(
     n_jobs=8,
 )
 
-write_on_file(grid_search_results, 'results/ml_cup_choose_seed_0-7')
+write_on_file(grid_search_results, 'results/ml_cup_choose_seed_8-15')
